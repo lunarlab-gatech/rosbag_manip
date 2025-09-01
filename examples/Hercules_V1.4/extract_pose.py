c35bd2ffc15b823bde7f80807b116bea6b61090e
Daniel Butterfield <danielcbutter@gmail.com>
2025-09-01 10:29:15 -0400

    Additional example tweaks

--- examples/Hercules_V1.4/extract_pose.py
+++ examples/Hercules_V1.4/extract_pose.py
@@ -4,7 +4,7 @@ def main():
     robot_name = "Drone2"
 
     # Load the odometry data
-    odom_data = OdometryData.from_txt_file('/media/dbutterfield3/T73/Hercules_datasets/V1.4.1/data/' + robot_name + '/odom.txt', robot_name + '/odom', robot_name + '/ground_truth/base_link', CoordinateFrame.NED)
+    odom_data = OdometryData.from_txt_file('/media/dbutterfield3/T73/Hercules_datasets/V1.4.1/data/' + robot_name + '/pose_world_frame.txt', robot_name + '/odom', robot_name + '/ground_truth/base_link', CoordinateFrame.NED)
 
     # Convert to the FLU coordinate frame
     odom_data.to_FLU_frame()
--- examples/visualize_odom.py
+++ examples/visualize_odom.py
@@ -7,13 +7,12 @@ def main():
     # Load csv files
     data: list[OdometryData] = []
     for name in robot_names:
-        d = OdometryData.from_csv('/media/dbutterfield3/T73/Hercules_datasets/V1.4/extract/files_for_roman_baseline/'+name+'/poseGT.csv', "odom", 'ground_truth/base_link', CoordinateFrame.FLU, True, None)
+        d = OdometryData.from_csv('/media/dbutterfield3/T73/Hercules_datasets/V1.4.1/extract/files_for_roman_baseline/'+name+'/poseGT.csv', "odom", 'ground_truth/base_link', CoordinateFrame.FLU, True, None)
         data.append(d)
 
     # Load VINS estimated results
     for name in robot_names:
-        print(name)
-        d = OdometryData.from_csv('/media/dbutterfield3/T73/Hercules_datasets/V1.4/extract/files_for_roman_baseline/'+name+'/vins_result_no_loop_reformatted.csv', "odom", 'base_link', CoordinateFrame.FLU, True, None)
+        d = OdometryData.from_csv('/media/dbutterfield3/T73/Hercules_datasets/V1.4.1/extract/files_for_roman_baseline/'+name+'/vins_result_no_loop_reformatted.csv', "odom", 'base_link', CoordinateFrame.FLU, True, None)
         data.append(d)
 
     data[0].shift_to_start_at_identity()
